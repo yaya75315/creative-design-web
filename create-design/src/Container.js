6c241913f7776e5322db15d9b1894f5f679ed93d
SylviaWang <yaya75315@gmail.com>
2021-01-28 20:26:39 +0800

    container

---
+++ create-design/src/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Container = () => {
+    console.log('container');
+    return (
+        <div className={homeContainer}>
+
+        </div>
+    )
+}
+
+export default Container
